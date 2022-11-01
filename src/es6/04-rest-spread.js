d71931651ab3a6414c3fdb3955a98a2e5aa81741
aaxlss <axl9305@gmail.com>
2022-11-01 12:39:51 +0000

    [Update] Adding spread operators and rest functions

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -12,3 +12,20 @@ let user = {
 let {name, age} = user;
 console.log(name, age);
 console.log(user.name, user.age);
+
+//spread operator
+let person = {name: 'Axel', age:29};
+let country = 'Mx';
+
+let data = {...person, country}
+console.log(data)
+
+//rest 
+const sum = (num, ...values) =>{
+    console.log(values);
+    console.log(num + values[0]);
+
+    return num + values[0]
+}
+
+sum(1,2,456,89,23,86,23)
