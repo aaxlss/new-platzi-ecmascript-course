e2fb4c7e9ab7eb979eaffff39850fbca74040474
aaxlss <axl9305@gmail.com>
2022-11-01 12:33:03 +0000

    [Add] Adding file explaning how to destructurade arrays and objects

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,14 @@
+let fruits = ['apple', 'banana']
+let [a,b] = fruits
+
+console.log(a, b)
+
+
+//object
+let user = {
+    name: 'Axel',
+    age: 29
+}
+let {name, age} = user;
+console.log(name, age);
+console.log(user.name, user.age);
