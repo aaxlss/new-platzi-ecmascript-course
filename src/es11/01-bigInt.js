e72d6c2a18026dfaad886927f3dbe6b1251de586
aaxlss <axl9305@gmail.com>
2022-11-02 10:43:10 +0000

    [Add] adding bigint example [Add] adding nullish example

---
+++ src/es11/01-bigInt.js
@@ -0,0 +1,5 @@
+const aBigNumber = 787897987897897897878789789798798987897n;
+const anotherBigNumber = BigInt(7897979787987979797987987)
+
+console.log(aBigNumber)
+console.log(anotherBigNumber)
---
+++ src/es11/02-nullish.js
@@ -0,0 +1,9 @@
+const number = 2;
+const number1 = null;
+
+//if the variable has a value, it will assign the result if not the result will be the value after ?? 
+const result = number ?? 4;
+const result1 = number1 ?? 56;
+
+console.log(result)
+console.log(result1)
