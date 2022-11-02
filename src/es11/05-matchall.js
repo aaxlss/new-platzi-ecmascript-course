cf66751bfbef4a77422a9dad0fdd025cf8dc5386
aaxlss <axl9305@gmail.com>
2022-11-02 13:13:22 +0000

    [Add] Adding the different ways to work with window, self and global variables [Add] Adding matchall method to use with regext to find the number of times that the regex match in a string

---
+++ src/es11/05-matchall.js
@@ -0,0 +1,8 @@
+//filering information using regex
+const regex = /\b(Apple)+\b/g;
+
+const fruit = "Apple, Banana, Kiwi, Apple, Orange, etc. etc. etc."
+
+const array = [...fruit.matchAll(regex)]
+
+console.log(array);
