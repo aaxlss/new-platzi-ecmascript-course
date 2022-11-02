4619ac3aa3143ac21e9d8c7156add1e44ad483f7
aaxlss <axl9305@gmail.com>
2022-11-02 13:49:43 +0000

    [Add] Showing numeric separators [Add] using replace all metod

---
+++ src/es12/01-replace-all.js
@@ -0,0 +1,5 @@
+const texto = 'Javascript es el mejor lenguaje. Javascript puede vivir en el navegador y en el servidor.'
+
+texto.replace('Javascript', 'TypeScript'); 
+
+texto.replaceAll('Javascript', 'TypeScript');
